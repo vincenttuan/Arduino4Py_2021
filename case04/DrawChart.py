834a7b5492d726e21e7727eed08881c0e9558c14
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 16:49:24 +0800

    Hello Python

---
+++ case04/DrawChart.py
@@ -0,0 +1,13 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect('../case03/demo.db')
+df = pd.read_sql_query("SELECT NAME, SALARY FROM Employee", con=conn)
+print(df)
+
+# 繪圖
+plt.plot(df['NAME'], df['SALARY']) # 繪製折線圖
+plt.show()
+
+conn.close()
