f7cf0ee287f6fb5f1558d6464e6034da2eb57183
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 13:17:08 +0800

    Hello Python

---
+++ case02/Decorated2.py
@@ -0,0 +1,22 @@
+# 裝飾器 2
+
+def make_dress(func):
+    def dress():
+        print("穿衣服")
+        func()
+    return dress
+
+def make_shoes(func):
+    def dress():
+        print("穿鞋子")
+        func()
+    return dress
+
+@make_dress
+@make_shoes
+def out():
+    print("我出門了")
+
+if __name__ == '__main__':
+    john = out
+    john()
