e0d6714d2573951ce03442b8dc6e0c6f4cd53158
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 16:37:42 +0800

    Hello Python

--- case04/QueryTable.py
+++ case04/QueryTable.py
@@ -1,1 +1,13 @@
 # 請求出最高薪與最低薪的人名 ?
+import sqlite3
+sql = '''
+        select 'MAX' AS TYPE, name, max(salary) as SALARY from Employee
+        UNION ALL
+        select 'MIN' AS TYPE, name, min(salary) as SALARY from Employee
+    '''
+
+conn = sqlite3.connect('../case03/demo.db')
+cursor = conn.cursor()
+cursor.execute(sql)
+rows = cursor.fetchall()
+print(rows)
