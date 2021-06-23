11c944005e70c9a10ea27878917b0c56728f302c
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 16:58:56 +0800

    Hello Python

--- case04/DrawChart.py
+++ case04/DrawChart.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,8 +8,15 @@ df = pd.read_sql_query("SELECT NAME, SALARY FROM Employee", con=conn)
 print(df)
 
 # 繪圖
+ma = df['SALARY'].rolling(window=2).mean()  # window=2 (二點算出一個平均值)移動平均值
+print(ma)
 plt.plot(df.NAME.values, df.SALARY.values, 'r.')  # 點紅點
 plt.plot(df['NAME'], df['SALARY'])  # 繪製折線圖
+plt.plot(df['NAME'], ma)  # 繪製移動平均線折線圖
+# 圖例
+plt.xlabel('Name')
+plt.ylabel('Salary')
+
 plt.show()
 
 conn.close()
