861f7c31b33b7f23593aafa9ba0c41b7df945497
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 13:06:30 +0800

    Hello Python

--- case02/LambdaLab.py
+++ case02/LambdaLab.py
@@ -6,3 +6,21 @@ id = 'A123456789'
 印出: 台灣男
 '''
 id = "A123456789"
+sex = id[1]
+area = id[2]
+sex_info = {1: lambda : "男", 2: lambda : "女"}
+area_info = {
+                0: lambda: "台灣",
+                1: lambda: "台灣",
+                2: lambda: "台灣",
+                3: lambda: "台灣",
+                4: lambda: "台灣",
+                5: lambda: "台灣",
+                6: lambda: "外國",
+                7: lambda: "無戶籍",
+                8: lambda: "港澳",
+                9: lambda: "大陸"
+             }
+
+print(area, sex)
+print(area_info.get(int(area))(), sex_info.get(int(sex))())
