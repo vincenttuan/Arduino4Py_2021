b0650fbd8ee4f5fcd1f95574087fc632fcffd743
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 16:50:56 +0800

    Hello Python

--- case04/DrawChart.py
+++ case04/DrawChart.py
@@ -7,7 +7,8 @@ df = pd.read_sql_query("SELECT NAME, SALARY FROM Employee", con=conn)
 print(df)
 
 # 繪圖
-plt.plot(df['NAME'], df['SALARY']) # 繪製折線圖
+plt.plot(df.NAME.values, df.SALARY.values, 'r.')  # 點紅點
+plt.plot(df['NAME'], df['SALARY'])  # 繪製折線圖
 plt.show()
 
 conn.close()
