937beaf8af6fa5b741e304ad6ce54a00bfbbc3c1
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 11:35:29 +0800

    Hello Python

---
+++ case02/EnclosingDef3.py
@@ -0,0 +1,18 @@
+# 嵌套函式 3
+# 多重相乘
+
+def multi(n):
+    if n == 0:
+        return None
+
+    def multi(x):
+        return n * x
+
+    return multi  # 得到第8行的方法參考
+
+if __name__ == '__main__':
+    n3 = multi(3)
+    n5 = multi(5)
+    x = 6
+    print(n3(x)) # x = 6
+    print(n3(n5(x))) # n3(n5(x)) -> n3(n5(6)) -> n3(30) -> 90
