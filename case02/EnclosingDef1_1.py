99ba2a8c89bf426c095a9b86f52884c1559c46e7
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 11:19:52 +0800

    Hello Python

---
+++ case02/EnclosingDef1_1.py
@@ -0,0 +1,15 @@
+# 嵌套函式 1
+
+def mssage(text):
+    text = text + " by 巨匠電腦"
+    print("mssage 方法執行")
+    def print_message():
+        print("print_message 方法執行")
+        print(text)
+
+    return print_message()
+
+
+if __name__ == '__main__':
+    m1 = mssage("Hello")
+    print(m1)
