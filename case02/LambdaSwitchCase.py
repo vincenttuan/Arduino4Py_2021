b00f69f6769e61bf710a64631b456926ad682a39
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-23 11:03:25 +0800

    Hello Python

--- case02/LambdaSwitchCase.py
+++ case02/LambdaSwitchCase.py
@@ -1,7 +1,7 @@
 # python 沒有 switch case
 # lambda + dict 合作
 
-id = "A123456789"
+id = "A223456789"
 sex = id[1]
 print(sex)
 
@@ -11,3 +11,10 @@ sex_info = {
 }
 sex_error = lambda : print("性別錯誤")
 sex_info.get(int(sex), sex_error)()
+
+salary_weight = {
+    1: lambda salary: print("男", salary),
+    2: lambda salary: print("女", salary * 1.2)
+}
+sex_error = lambda : print("性別錯誤")
+salary_weight.get(int(sex), sex_error)(30000)
