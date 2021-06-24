087ccc228bb72e50bd57638d3905c85667c59063
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-24 16:39:13 +0800

    Hello Python

--- case05/TWII_個股.py
+++ case05/TWII_個股.py
@@ -4,10 +4,14 @@ from io import StringIO
 import matplotlib.pyplot as plt
 
 symbol = "2330"
+date = "20210531"
+path = "https://www.twse.com.tw/exchangeReport/BWIBBU?response=csv&date=%s&stockNo=%s" % (date, symbol)
+csv = requests.get(path).text
+
 date = "20210624"
 path = "https://www.twse.com.tw/exchangeReport/BWIBBU?response=csv&date=%s&stockNo=%s" % (date, symbol)
+csv = csv + requests.get(path).text
 
-csv = requests.get(path).text
 # "日期","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
 data = csv.split('\r\n')
 data = list(filter(lambda l: len(l.split(',')) == 7, data )) # 過濾
@@ -21,5 +25,5 @@ df = df.rename(columns={'殖利率(%)':'殖利率'})
 df = df.rename(columns={'財報年/季':'財報年季'})
 print(df)
 
-plt.plot(df['日期'], df['殖利率(%)'])  # 繪製折線圖
+plt.plot(df['日期'], df['股價淨值比'])  # 繪製折線圖
 plt.show()
