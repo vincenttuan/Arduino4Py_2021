7148eb9bd8364de91f1da24cce51bfc9cf7a81a2
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-24 14:34:07 +0800

    Hello Python

--- case05/TWII.py
+++ case05/TWII.py
@@ -3,6 +3,24 @@ import requests
 path = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20210623&selectType=ALL'
 csv = requests.get(path).text
 # "證券代號","證券名稱","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
+csv = csv.replace('"1,', '"1')
+csv = csv.replace('"2,', '"2')
+csv = csv.replace('"3,', '"3')
 csv = csv.replace('"', '')
 csv = csv.replace('-', '-1')
-print(csv)
+rows = csv.split("\r\n")
+# "1,040.00" 1760
+print(len(rows))
+stocks = []
+for row in rows:
+    list = row.split(',')
+    if len(list) == 8 and list[0] != '證券代號':
+        #print(list)
+        list[2] = float(list[2])  # 殖利率(%)
+        list[3] = int(list[3])    # 股利年度
+        list[4] = float(list[4])  # 本益比
+        list[5] = float(list[5])  # 股價淨值比
+        stocks.append(tuple(list))
+
+print(stocks)
+
