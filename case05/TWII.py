eb8ee622ff23f0a1bc399e4c7ecf0f0429ada7fa
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-24 14:12:19 +0800

    Hello Python

--- case05/TWII.py
+++ case05/TWII.py
@@ -2,5 +2,7 @@ import requests
 
 path = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20210623&selectType=ALL'
 csv = requests.get(path).text
+# "證券代號","證券名稱","殖利率(%)","股利年度","本益比","股價淨值比","財報年/季",
+csv = csv.replace('"', '')
+csv = csv.replace('-', '-1')
 print(csv)
-
