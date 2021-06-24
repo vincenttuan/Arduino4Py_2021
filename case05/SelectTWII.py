b77b72c4e1d3f3ef33f1bc7500e20b8994f0fc8e
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-24 15:21:23 +0800

    Hello Python

--- case05/SelectTWII.py
+++ case05/SelectTWII.py
@@ -4,17 +4,24 @@ import sqlite3
 殖利率(r): >= 7%
 股價淨值比(pb): 小於 1 適合買進, 大於等於 1 適合賣出
 '''
-bs = int(input('買進(1)賣出(2):'))
-pe = float(input('請輸入本益比:'))
-r  = float(input('請輸入殖利率:'))
+bs = int(input('買進(1)賣出(2)查看股票(3): '))
+if bs == 3:
+    symbol = input('請輸入股票代號: ')
+    sql = '''
+            select 證券代號, 證券名稱, 本益比, 殖利率, 股價淨值比, ts from Stock
+            where 證券代號 = '%s'
+          ''' % symbol
+else:
+    pe = float(input('請輸入本益比: '))
+    r  = float(input('請輸入殖利率: '))
+    # 殖利率(%)","本益比","股價淨值比
+    sql = '''
+            select 證券代號, 證券名稱, 本益比, 殖利率, 股價淨值比, ts from Stock
+            where (本益比 <= %.1f and 本益比 > 0) and 
+                  (殖利率 >= %.1f and 殖利率 > 0) and 
+                  (股價淨值比 %s 1 and 股價淨值比 > 0)
+          ''' % (pe, r, '<' if bs == 1 else '>=')
 
-# 殖利率(%)","本益比","股價淨值比
-sql = '''
-        select 證券代號, 證券名稱, 本益比, 殖利率, 股價淨值比 from Stock
-        where (本益比 <= %.1f and 本益比 > 0) and 
-              (殖利率 >= %.1f and 殖利率 > 0) and 
-              (股價淨值比 %s 1 and 股價淨值比 > 0)
-      ''' % (pe, r, '<' if bs == 1 else '>=')
 print(sql)
 
 conn = sqlite3.connect('twii.db')
@@ -22,7 +29,7 @@ cursor = conn.cursor()
 cursor.execute(sql)
 results = cursor.fetchall()
 
-print('證券代號, 證券名稱, 本益比, 殖利率, 股價淨值比')
+print('證券代號, 證券名稱, 本益比, 殖利率, 股價淨值比, 日期')
 for result in results:
     print(result)
 
