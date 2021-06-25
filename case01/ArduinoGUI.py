8aaa769e8ce0f0f489635edc1fd358a766bbde2a
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-25 16:23:57 +0800

    Hello Python

--- case01/ArduinoGUI.py
+++ case01/ArduinoGUI.py
@@ -39,6 +39,12 @@ def receiveData():
                 elif (int(values[3]) == 32):
                     sendButton0.config(image=buzeer_close_photo)
                     sendButton0.image = buzeer_close_photo
+                if (int(values[4]) == 15):
+                    sendButton4.config(image=door_close_photo)
+                    sendButton4.image = door_close_photo
+                elif (int(values[4]) == 105):
+                    sendButton4.config(image=door_open_photo)
+                    sendButton4.image = door_open_photo
             except:
                 pass
 
@@ -59,6 +65,13 @@ def sendData(n):
     data = data_row.encode()
     ser.write(data)
     print("send: ", data_row, data)
+
+def openTheDoor():
+    door = int(data.split(",")[4])
+    if door == 15 :
+        sendData('8')
+    elif door == 105:
+        sendData('4')
 
 def getOpenWeatherData():
     status_code, main, icon, temp, feels_like, humidity = ow.openweather()
@@ -135,8 +148,7 @@ if __name__ == '__main__':
     sendButton1  = tkinter.Button(text='1', image=red_photo, command=lambda: sendData('1'), font=myfont2)
     sendButton2  = tkinter.Button(text='2', image=green_photo, command=lambda: sendData('2'), font=myfont2)
     sendButton3  = tkinter.Button(text='3', image=yellow_photo, command=lambda: sendData('3'), font=myfont2)
-    sendButton4  = tkinter.Button(text='4', image=door_close_photo, command=lambda: sendData('4'), font=myfont2)
-    sendButton8  = tkinter.Button(text='8', image=door_open_photo, command=lambda: sendData('8'), font=myfont2)
+    sendButton4  = tkinter.Button(text='4', image=door_close_photo, command=lambda: openTheDoor(), font=myfont2)
     # 爬蟲 Openweather -----------------------------------------------------------------
     owmainButton = tkinter.Button(textvariable=owmainValue, command=lambda: getOpenWeatherData(), font=myfont2)
     owiconLabel = tkinter.Label(root, image=None)
@@ -156,8 +168,7 @@ if __name__ == '__main__':
     sendButton1.grid(row=0,   column=1, columnspan=1, sticky='EWNS')
     sendButton2.grid(row=0,   column=2, columnspan=1, sticky='EWNS')
     sendButton3.grid(row=0,   column=3, columnspan=1, sticky='EWNS')
-    sendButton4.grid(row=0,   column=4, columnspan=1, sticky='EWNS')
-    sendButton8.grid(row=0,   column=5, columnspan=1, sticky='EWNS')
+    sendButton4.grid(row=0,   column=4, columnspan=2, sticky='EWNS')
     # 爬蟲 Openweather -----------------------------------------------------------------
     owiconLabel.grid(row=1, column=0, columnspan=1, sticky='EWNS')
     owmainButton.grid(row=1, column=1, columnspan=2, sticky='EWNS')
@@ -168,10 +179,14 @@ if __name__ == '__main__':
     cdsLabel.grid(row=2,  column=0, columnspan=2, sticky='EWNS')
     tempLabel.grid(row=2, column=2, columnspan=2, sticky='EWNS')
     humiLabel.grid(row=2, column=4, columnspan=2, sticky='EWNS')
+    # ----------------------------------------------------------------------------------
     receiveLabel.grid(row=3,  column=0, columnspan=6, sticky='EWNS')
 
     t1 = threading.Thread(target=receiveData)
     t1.start()
+
+    t2 = threading.Thread(target=getOpenWeatherData)
+    t2.start()
 
     root.mainloop()
 
