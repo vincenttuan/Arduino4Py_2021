c46672cd3c284147f17507a4e71de23cf5fbd117
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-25 10:04:14 +0800

    Hello Python

--- case01/ArduinoGUI.py
+++ case01/ArduinoGUI.py
@@ -49,6 +49,8 @@ def sendData(n):
     ser.write(data)
     print("send: ", data_row, data)
 
+def getOpenWeatherData():
+    pass
 
 if __name__ == '__main__':
 
@@ -64,9 +66,22 @@ if __name__ == '__main__':
     myfont1 = font.Font(family='Helvetica', size=36, weight='bold')
     myfont2 = font.Font(family='Helvetica', size=24)
 
-    respText = tkinter.StringVar()
-    respText.set("0,0.0,0.0")
+    # 爬蟲 Openweather -----------------------------------------------------------------
+    owmainValue = tkinter.StringVar()
+    owmainValue.set("")
 
+    owiconValue = tkinter.StringVar()
+    owiconValue.set("")
+
+    owtempValue = tkinter.StringVar()
+    owtempValue.set("")
+
+    owfeelsLikeValue = tkinter.StringVar()
+    owfeelsLikeValue.set("")
+
+    owhumidityValue = tkinter.StringVar()
+    owhumidityValue.set("")
+    # ----------------------------------------------------------------------------------
     cdsValue = tkinter.StringVar()
     cdsValue.set("0 lu")
 
@@ -76,18 +91,28 @@ if __name__ == '__main__':
     humiValue = tkinter.StringVar()
     humiValue.set("00.00%")
 
+    respText = tkinter.StringVar()
+    respText.set("0,0.0,0.0")
+
     sendButton0  = tkinter.Button(text='0', command=lambda: sendData('0'), font=myfont2)
     sendButton1  = tkinter.Button(text='1', command=lambda: sendData('1'), font=myfont2)
     sendButton2  = tkinter.Button(text='2', command=lambda: sendData('2'), font=myfont2)
     sendButton3  = tkinter.Button(text='3', command=lambda: sendData('3'), font=myfont2)
     sendButton4  = tkinter.Button(text='4', command=lambda: sendData('4'), font=myfont2)
     sendButton8  = tkinter.Button(text='8', command=lambda: sendData('8'), font=myfont2)
-    receiveLabel = tkinter.Label(root, textvariable=respText)
+    # 爬蟲 Openweather -----------------------------------------------------------------
+    owmainButton = tkinter.Button(text='', command=lambda: getOpenWeatherData(), font=myfont2)
+    owiconLabel = tkinter.Label(root, textvariable=owiconValue, font=myfont2, fg='#ff0000')
+    owtempLabel = tkinter.Label(root, textvariable=owtempValue, font=myfont2, fg='#ff0000')
+    owfeelsLikeLabel = tkinter.Label(root, textvariable=owfeelsLikeValue, font=myfont2, fg='#ff0000')
+    owhumidityLabel = tkinter.Label(root, textvariable=owhumidityValue, font=myfont2, fg='#ff0000')
+    # ----------------------------------------------------------------------------------
     cdsLabel = tkinter.Label(root, textvariable=cdsValue, font=myfont1, fg='#ff0000')
     tempLabel = tkinter.Label(root, textvariable=tempValue, font=myfont1, fg='#005100')
     humiLabel = tkinter.Label(root, textvariable=humiValue, font=myfont1, fg='#00f')
+    receiveLabel = tkinter.Label(root, textvariable=respText)
 
-    root.rowconfigure((0,1), weight=1) # 列 0, 列 1 同步放大縮小
+    root.rowconfigure((0,1,2), weight=1) # 列 0, 列 1 同步放大縮小
     root.columnconfigure((0,1,2,3,4,5), weight=1) # 欄 0, 欄 1, 欄 2 ...同步放大縮小
 
     sendButton0.grid(row=0,   column=0, columnspan=1, sticky='EWNS')
@@ -96,10 +121,17 @@ if __name__ == '__main__':
     sendButton3.grid(row=0,   column=3, columnspan=1, sticky='EWNS')
     sendButton4.grid(row=0,   column=4, columnspan=1, sticky='EWNS')
     sendButton8.grid(row=0,   column=5, columnspan=1, sticky='EWNS')
-    cdsLabel.grid(row=1,  column=0, columnspan=2, sticky='EWNS')
-    tempLabel.grid(row=1, column=2, columnspan=2, sticky='EWNS')
-    humiLabel.grid(row=1, column=4, columnspan=2, sticky='EWNS')
-    receiveLabel.grid(row=2,  column=0, columnspan=6, sticky='EWNS')
+    # 爬蟲 Openweather -----------------------------------------------------------------
+    owmainButton.grid(row=1, column=0, columnspan=2, sticky='EWNS')
+    owiconLabel.grid(row=1, column=2, columnspan=1, sticky='EWNS')
+    owtempLabel.grid(row=1, column=3, columnspan=1, sticky='EWNS')
+    owfeelsLikeLabel.grid(row=1, column=4, columnspan=1, sticky='EWNS')
+    owhumidityLabel.grid(row=1, column=5, columnspan=1, sticky='EWNS')
+    # ----------------------------------------------------------------------------------
+    cdsLabel.grid(row=2,  column=0, columnspan=2, sticky='EWNS')
+    tempLabel.grid(row=2, column=2, columnspan=2, sticky='EWNS')
+    humiLabel.grid(row=2, column=4, columnspan=2, sticky='EWNS')
+    receiveLabel.grid(row=3,  column=0, columnspan=6, sticky='EWNS')
 
     t1 = threading.Thread(target=receiveData)
     t1.start()
