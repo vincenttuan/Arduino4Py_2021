279944855e5e3d05e723d394c1d863188e8f0682
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-25 09:40:14 +0800

    Hello Python

--- case01/OpenWeather.py
+++ case01/OpenWeather.py
@@ -1,9 +1,19 @@
 import requests
+import json
 
-city = 'taoyuan'
-country = 'tw'
-apikey = '3b657e4dc92918d9d95fff4633377535'
-url = 'api.openweathermap.org/data/2.5/weather?q={},{}&appid={}'\
-      .format(city, country, apikey)
+def openweather():
+    city = 'taoyuan'
+    country = 'tw'
+    apikey = '3b657e4dc92918d9d95fff4633377535'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}'\
+          .format(city, country, apikey)
 
-print(url)
+    resp = requests.get(url)
+    if(resp.status_code == 200):
+        jo = json.loads(resp.text)
+        print(jo)
+    else:
+        print('Error', resp.status_code)
+
+if __name__ == '__main__':
+    openweather()
