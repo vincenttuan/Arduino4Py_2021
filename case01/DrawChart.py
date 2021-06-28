d49ea84c02716720ac830b4350801b502e443b34
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-28 15:41:03 +0800

    Hello Python

--- case01/DrawChart.py
+++ case01/DrawChart.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 conn = sqlite3.connect('iot.db')
 df = pd.read_sql_query("SELECT id, cds, temp, humi, ts FROM Env "
-                       "order by ts desc limit 50", con=conn)
+                       "order by ts desc limit 15", con=conn)
 df = df[::-1] # reverse
 print(df)
 
@@ -13,13 +13,14 @@ print(df)
 #ma = df['SALARY'].rolling(window=2).mean()  # window=2 (二點算出一個平均值)移動平均值
 #print(ma)
 #plt.plot(df.ts.values, df.temp.values, 'r.')  # 點紅點
-plt.plot(df['ts'], df['temp'])  # 繪製折線圖
-plt.plot(df['ts'], df['humi'])  # 繪製折線圖
+plt.plot(df['ts'], df['temp'], label="temp")  # 繪製折線圖
+plt.plot(df['ts'], df['humi'], label="humi")  # 繪製折線圖
+plt.grid(True)
 #plt.plot(df['NAME'], ma)  # 繪製移動平均線折線圖
 # 圖例
 # plt.xlabel('Name')
 # plt.ylabel('Salary')
-
+plt.legend()
 plt.show()
 
 conn.close()
