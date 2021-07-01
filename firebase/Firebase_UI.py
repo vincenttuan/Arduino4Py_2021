4bfb9befd0d46bf5e33a0395bd84be81b3202397
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-01 16:42:28 +0800

    Hello Python

--- firebase/Firebase_UI.py
+++ firebase/Firebase_UI.py
@@ -3,3 +3,66 @@
 | 25.4 | 45.1 | off |
 +------+------+-----+
 '''
+import tkinter
+from tkinter import font
+from PIL import Image, ImageTk
+import threading
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate('key.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://arduino-iot-202107-default-rtdb.firebaseio.com/'
+})
+
+def listenerDoor(event):
+    print(event.data)
+    doorValue.set(event.data)
+
+def listenerDHT11Temp(event):
+    print(event.data)
+    tempValue.set(event.data)
+
+def listenerDHT11Humi(event):
+    print(event.data)
+    humiValue.set(event.data)
+
+def listenerFirebase():
+    firebase_admin.db.reference("/door").listen(listenerDoor)
+    firebase_admin.db.reference("/dht11/temp").listen(listenerDHT11Temp)
+    firebase_admin.db.reference("/dht11/humi").listen(listenerDHT11Humi)
+
+root = tkinter.Tk()
+root.geometry("500x150")
+root.title("Firebase console")
+
+myfont = font.Font(family='Helvetica', size=36, weight='bold')
+
+door_open_photo  = ImageTk.PhotoImage(Image.open('door_open.png'))
+door_close_photo = ImageTk.PhotoImage(Image.open('door_close.png'))
+
+tempValue = tkinter.StringVar()
+tempValue.set("00.0 C")
+
+humiValue = tkinter.StringVar()
+humiValue.set("00.0 %")
+
+doorValue = tkinter.StringVar()
+doorValue.set("0")
+
+tempLabel = tkinter.Label(root, textvariable=tempValue, font=myfont)
+humiLabel = tkinter.Label(root, textvariable=humiValue, font=myfont)
+doorLabel = tkinter.Label(root, textvariable=doorValue, font=myfont)
+
+root.rowconfigure(0, weight=1)
+root.columnconfigure((0, 1, 2), weight=1)
+
+tempLabel.grid(row=0, column=0, sticky='EWNS')
+humiLabel.grid(row=0, column=1, sticky='EWNS')
+doorLabel.grid(row=0, column=2, sticky='EWNS')
+
+t1 = threading.Thread(target=listenerFirebase)
+t1.start()
+
+root.mainloop()
